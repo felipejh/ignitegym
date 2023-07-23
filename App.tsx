3bb2fb2258682410a26d1c80c82948f0bc0401f6
Felipe Hoffmann <felipejh@gmail.com>
2023-07-23 11:17:36 -0300

    chore: configure eslint and prettier

--- App.tsx
+++ App.tsx
@@ -1,12 +1,20 @@
-import { Text, View } from 'react-native';
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
+import { Text, View } from 'react-native'
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
 
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      {fontsLoaded ? <Text style={{ fontFamily: 'Roboto_700Bold' }}>Hello world!</Text> : <View />}      
+      {fontsLoaded ? (
+        <Text style={{ fontFamily: 'Roboto_700Bold' }}>Hello world!</Text>
+      ) : (
+        <View />
+      )}
     </View>
-  );
+  )
 }
